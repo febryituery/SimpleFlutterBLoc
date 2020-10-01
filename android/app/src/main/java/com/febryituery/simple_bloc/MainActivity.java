3ab4ceee0a936dfe85d313b079996a3d2df86376
Febry Dwi Putra <febryituery@gmail.com>
2020-10-01 11:09:39 +0700

    [Febry] first commit

---
+++ android/app/src/main/java/com/febryituery/simple_bloc/MainActivity.java
@@ -0,0 +1,6 @@
+package com.febryituery.simple_bloc;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/counter_event.dart
@@ -0,0 +1,5 @@
+abstract class CounterEvent {}
+
+class EventIncrement extends CounterEvent {}
+
+class EventDecrement extends CounterEvent {}
